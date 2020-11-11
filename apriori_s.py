a7697aff1ca92e5d24a9c59e1b78111c5bd41b4c
Piotr Danielczyk <piotrekdanielczyk@gmail.com>
2020-11-11 12:00:18 +0100

    Save length of sequence in dataset for optimization

--- apriori_s.py
+++ apriori_s.py
@@ -1,3 +1,30 @@
+def add_len_of_sequence_to_dataset(dataset: []) -> []:
+    """To optimize performance and avoid checking of length of sequence every iteration, calculate the length once and
+    save it in the dataset (as element with index 2 in the tuple)"""
+    dataset_with_lengths = []
+    for sequence_tuple in dataset:
+        length_of_sequence = len(sequence_tuple[1])
+        dataset_with_lengths.append(sequence_tuple + (length_of_sequence,))
+    return dataset_with_lengths
+
+
+def print_line_by_line(list_to_be_printed: []):
+    if len(list_to_be_printed):
+        for line in list_to_be_printed:
+            print(line)
+    else:
+        print("Empty")
+
+
+def apriori_s(dataset: [], min_sup: float):
+    print("Original dataset:")
+    print_line_by_line(dataset)
+
+    dataset_with_lengths = add_len_of_sequence_to_dataset(dataset)
+    print("\nDataset with added lengths of sequences (index number 2 in the tuple):")
+    print_line_by_line(dataset_with_lengths)
+
+
 def main():
     dataset = [
                 ('1', ['p1', 'p2', 'p5']),
@@ -17,7 +44,7 @@ def main():
                 ('15', ['p2', 'p1', 'p3', 'p4'])
     ]
     min_sup = 2
-    # apriori_s(dataset, min_sup)
+    apriori_s(dataset, min_sup)
 
 
 if __name__ == '__main__':
